13022c3fc76e0406d47c10067e9423cba4e815c4
Kevinoh47 <kohok47@gmail.com>
2019-01-29 21:10:23 -0800

    working socket.io chat buit with gatsby

---
+++ chat-client/src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chatter from "./chatter.js";
+
+const App = props => {
+  return (
+    <React.Fragment>
+      <Chatter />
+      <hr />
+    </React.Fragment>
+  );
+};
+
+export default App;
